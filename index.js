89357559add7c9d4f406d9b31746188f85c5ca36
jeojoe <jirat.onaree@gmail.com>
2018-03-17 17:56:01 +0700

    Add superagent and create client + server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = () => 'lol';
+const { buffer } = require('micro');
+const cors = require('micro-cors')();
+
+const handler = async (req) => {
+  const data = await buffer(req);
+  console.log(data);
+  return 'success!!';
+};
+
+module.exports = cors(handler);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import request from 'superagent';
+import config from '../config';
 
-export default () => <div>lollollol</div>;
+export default class Home extends Component {
+  state = {
+    file: null,
+  }
+
+  _uploadFile = (file) => {
+    console.log(file);
+    request
+      .post(config.SERVER_URL)
+      .send(file)
+      .end((err, res) => console.log(res, err));
+  }
+
+  _formSubmit = (e) => {
+    e.preventDefault();
+    this._uploadFile(this.state.file);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this._formSubmit}>
+        <input
+          type="file"
+          onChange={e => this.setState({ file: e.target.files[0] })}
+        />
+        <button type="submit">Upload!</button>
+      </form>
+    );
+  }
+}
