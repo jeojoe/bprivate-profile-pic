63e70eb0f53969252f4a4f38eb0c567348d6c66e
jeojoe <jirat.onaree@gmail.com>
2018-03-19 20:55:15 +0700

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,9 @@ const Layout = ({ children }) => (
       <title>Bitcoin Private Profile Pic</title>
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700" rel="stylesheet" />
+      {/* Favicon */}
+      <link rel="icon" type="image/png" href="static/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="static/favicon-16x16.png" sizes="16x16" />
     </Head>
     {children}
   </div>
